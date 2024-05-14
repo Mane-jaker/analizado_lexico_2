74dba11d93d4ce509a01933473701af71ef32f28
Mane <angelillo3581@gmail.com>
2024-05-14 10:59:17 -0600

    folders

--- analizador_lexico.py
+++ analizador_lexico.py
@@ -12,14 +12,22 @@ tokens = [
     'ID',
     'LPAREN',
     'RPAREN',
+    'SEMICOLON',
+    'COMMA',
+    'LBRACE',
+    'RBRACE'
 ] + list(reserved.values())
 
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+t_SEMICOLON = r';'
+t_COMMA = r','
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'ID')
+    t.type = reserved.get(t.value, 'ID')  # Check reserved words
     return t
 
 t_ignore = ' \t'
